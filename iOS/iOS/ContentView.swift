ca047c832096348066b55318df4ac1600865e9b7
Elliott Larsen <elliottlrsn@gmail.com>
2023-07-02 11:55:52 -0600

    Frontend color added.

--- iOS/iOS/ContentView.swift
+++ iOS/iOS/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var movieList: MovieList?
     @State private var recommendations: Recommendations?
     @State private var selected_title = ""
@@ -17,44 +16,50 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        VStack {
-            // Movie Picker from the GET /movies endpoint.
-            Picker("movie_title", selection: $selected_title) {
-                ForEach(movieList?.titles ?? [""], id: \.self) {
-                    Text($0)
-                }
-            }
-            .pickerStyle(.wheel)
-            
-            
-            Button(action: {getRecommendations(selected_title: selected_title)}) {
-                // Calls GET /movies/{title}.
-                Text("Search")
-                    .bold()
-                    .padding(20)
-                    .foregroundColor(Color.white)
-                    .background(Color.black)
-                    .cornerRadius(15)
-            }
-            
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(recommendations?.images ?? [""], id: \.self) {
-                        // Displays the movie posters.
-                        AsyncImage(url: URL(string: $0)) { image in image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            Color.white
-                        }
+        ZStack {
+            Color.background_color
+                .ignoresSafeArea()
+            VStack {
+                // Movie Picker from the GET /movies endpoint.
+                Picker("movie_title", selection: $selected_title) {
+                    ForEach(movieList?.titles ?? [""], id: \.self) {
+                        Text($0)
+                            .foregroundColor(Color.text_color)
+                            .fontWeight(.bold)
                     }
                 }
-                .padding(.horizontal)
+                .pickerStyle(.wheel)
+                
+                
+                Button(action: {getRecommendations(selected_title: selected_title)}) {
+                    // Calls GET /movies/{title}.
+                    Text("Search")
+                        .bold()
+                        .padding(10)
+                        .foregroundColor(Color.background_color)
+                        .background(Color.button_color)
+                        .cornerRadius(15)
+                }
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(recommendations?.images ?? [""], id: \.self) {
+                            // Displays the movie posters.
+                            AsyncImage(url: URL(string: $0)) { image in image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                Color.background_color
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .frame(maxHeight: 500)
+                
+                
+                .onAppear(perform: getMovieList)
             }
-            .frame(maxHeight: 500)
-            
-
-            .onAppear(perform: getMovieList)
         }
     }
     
@@ -103,6 +108,12 @@ struct MovieList: Decodable {
 struct Recommendations: Decodable {
     var titles: [String]
     var images: [String]
+}
+
+extension Color {
+    static let background_color = Color("BackgroundColor")
+    static let text_color = Color("TextColor")
+    static let button_color = Color("ButtonColor")
 }
 
 struct ContentView_Previews: PreviewProvider {
